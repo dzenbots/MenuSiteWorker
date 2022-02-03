41cf3a142402c318f0d2372072421a54644279a3
dzenbots <vv.khritkin@gmail.com>
2022-02-03 11:10:56 +0300

    add admin filter

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,9 @@
 from aiogram import Dispatcher
 
+from .is_admin import AdminFilter
 from loader import dp
-# from .is_admin import AdminFilter
 
 
 if __name__ == "filters":
-    # dp.filters_factory.bind(AdminFilter)
+    dp.filters_factory.bind(AdminFilter)
     pass

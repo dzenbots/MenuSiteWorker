3f2310153ebb51f9de98b22d76698c7f389133e6
dzenbots <vv.khritkin@gmail.com>
2022-02-05 09:05:47 +0300

    some fixes

--- utils/menu_worker/menu_updater.py
+++ utils/menu_worker/menu_updater.py
@@ -1,11 +1,12 @@
 from aiogram import Dispatcher
-from aiogram.types import CallbackQuery
 
 from utils.menu_worker.site_worker import SiteWorker
 
 
-def update_tomorrow(call: CallbackQuery, dp: Dispatcher):
-    pass
+def update_tomorrow(dp: Dispatcher):
+    sw = SiteWorker(site_auth=dp.bot.get('config').misc.school_auth,
+                    folder_paths=dp.bot.get('config').misc.files_paths)
+    sw.copy_tomorrow_today()
 
 
 def update_today(dp: Dispatcher):

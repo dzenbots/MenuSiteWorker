89030553126a671ff3fabf1a3cd8da22ec283af4
dzenbots <vv.khritkin@gmail.com>
2022-02-05 09:08:13 +0300

    add TODO

--- utils/menu_worker/menu_updater.py
+++ utils/menu_worker/menu_updater.py
@@ -6,7 +6,7 @@ from utils.menu_worker.site_worker import SiteWorker
 def update_tomorrow(dp: Dispatcher):
     sw = SiteWorker(site_auth=dp.bot.get('config').misc.school_auth,
                     folder_paths=dp.bot.get('config').misc.files_paths)
-    sw.copy_tomorrow_today()
+    # TODO: code here!
 
 
 def update_today(dp: Dispatcher):
